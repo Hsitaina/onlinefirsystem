c1d5a6c21d24106c23e968e08dc6a4e09d830b88
Hsitaina <sitaniaharshit@gmail.com>
2023-08-13 16:28:46 +0530

    my fir website

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<center></center><b class="copyright">&copy; Online FIR System </b> All rights reserved.</center>
+		</div>
+	</div>
---
+++ users/includes/footer.php
@@ -0,0 +1,10 @@
+ <footer class="site-footer">
+    <center>
+          <div class="text-center">
+          Online FIR System
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+    </center>
+      </footer>
